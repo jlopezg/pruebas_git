f18b8c185a7b8aeac5350433b232e137a9f1f2f1
Jaison <yeison0317@windowslive.com>
2018-12-14 14:03:28 -0400

    se agrego el .format jaj

--- prueba_jaison.py
+++ prueba_jaison.py
@@ -1,7 +1,7 @@
 name = raw_input("Enter a name")
 age = int(raw_input("Enter a age"))
 
-print("Your age is: ") + (name)
+print("Your age is:{} ".format(name))
 
 print("HOla mundo")
 print("comencemos") 
